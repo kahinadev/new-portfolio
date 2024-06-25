61578048e0df773d649089355edd0561c18c7e34
Kahina Ouali <kahinaouali.pro@gmail.com>
2024-06-25 19:56:11 +0200

    Ajout de la section presentation

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,63 +1,63 @@
-// SIDEBAR 
+// // SIDEBAR 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const openButton = document.getElementById("open-sidenav");
-    const closeButton = document.getElementById("close-sidenav");
-    const sidenav = document.getElementById("sidenav");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const openButton = document.getElementById("open-sidenav");
+//     const closeButton = document.getElementById("close-sidenav");
+//     const sidenav = document.getElementById("sidenav");
 
-    openButton.addEventListener("click", () => {
-        sidenav.classList.add("open");
-    });
+//     openButton.addEventListener("click", () => {
+//         sidenav.classList.add("open");
+//     });
 
-    closeButton.addEventListener("click", () => {
-        sidenav.classList.remove("open");
-    });
+//     closeButton.addEventListener("click", () => {
+//         sidenav.classList.remove("open");
+//     });
 
-});
-
-
-// CHANGE BACKGROUND OF THE HEADER
-
-document.addEventListener("scroll", function () {
-
-    const header = document.getElementById("header");
-    const sections = document.querySelectorAll(".section");
-    let currentSection = null;
-
-    sections.forEach(section => {
-
-        const rect = section.getBoundingClientRect();
-
-        if (rect.top <= 60 && rect.bottom >= 60) {
-
-            currentSection = section;
-
-        }
-
-    });
-
-    if (window.scrollY === 0) {
-
-        header.style.background = "transparent";
-        header.classList.remove("light-header");
-        header.classList.remove("dark-header");
-
-    } else if (currentSection) {
-
-        if (currentSection.classList.contains("bg-light")) {
-
-            header.classList.add("light-header");
-            header.classList.remove("dark-header");
-            header.classList.remove("transparent-header");
-
-        } else if (currentSection.classList.contains("bg-dark")) {
-
-            header.classList.add("dark-header");
-            header.classList.remove("light-header");
-            header.classList.remove("transparent-header");
-
-        }
-    }
+// });
 
 
-}); 
+// // CHANGE BACKGROUND OF THE HEADER
+
+// document.addEventListener("scroll", function () {
+
+//     const header = document.getElementById("header");
+//     const sections = document.querySelectorAll(".section");
+//     let currentSection = null;
+
+//     sections.forEach(section => {
+
+//         const rect = section.getBoundingClientRect();
+
+//         if (rect.top <= 60 && rect.bottom >= 60) {
+
+//             currentSection = section;
+
+//         }
+
+//     });
+
+//     if (window.scrollY === 0) {
+
+//         header.style.background = "transparent";
+//         header.classList.remove("light-header");
+//         header.classList.remove("dark-header");
+
+//     } else if (currentSection) {
+
+//         if (currentSection.classList.contains("bg-light")) {
+
+//             header.classList.add("light-header");
+//             header.classList.remove("dark-header");
+//             header.classList.remove("transparent-header");
+
+//         } else if (currentSection.classList.contains("bg-dark")) {
+
+//             header.classList.add("dark-header");
+//             header.classList.remove("light-header");
+//             header.classList.remove("transparent-header");
+
+//         }
+//     }
+
+
+// }); 
